c84100fbbb4f6a0b7a41b8be5f17ce9d06f93461
Christopher Jones <crjones@gmail.com>
2012-04-13 15:28:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoutingIo::Application.config.secret_token = 'e1b1cdb36f942b6c8bc64c7a28381e1c08b2307ef137951a3abf8c234d37c6c5697025063a585a2fa4be9d6b1f1f9500261e0e00f6959748a03daac006edc779'
